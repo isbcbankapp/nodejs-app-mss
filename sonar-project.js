505023b813855599e0c6ba2b60e5b5cb28f3abea
rajilingam31 <132332789+rajilingam31@users.noreply.github.com>
2023-07-20 10:55:25 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.38.96.100:9000/',
+  serverUrl: 'http://13.235.95.72:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_0843edd7e8aa8bb8f2cd0b2fa624a1f76e5fdc96',
+	    'sonar.login': 'squ_f6c22f6c227397322a52d39892b162da5f9bb143',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
